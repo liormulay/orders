10926c42e03464c85cd45725204db833fb3a9606
liorm <liormualy6@gmail.com>
2020-06-04 14:03:48 +0300

    create AuthenticationEntryPointImp

---
+++ src/main/java/com/first/hello/configuration/AuthenticationEntryPointImp.java
@@ -0,0 +1,20 @@
+package com.first.hello.configuration;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class AuthenticationEntryPointImp implements AuthenticationEntryPoint , Serializable {
+
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+            throws IOException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+    }
+}
